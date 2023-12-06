f0009638483e20739f0b6dd10ec8fecc45b4d105
DarnishaTurner <darnishaturner@pursuit.org>
2023-12-05 20:39:31 -0500

    Darnishas Copy

---
+++ functions.js
@@ -0,0 +1,18 @@
+// Function to greet a person by name
+function sayHello(name) {
+    return `Hello ${name}!`;
+}
+
+// Calling the function with 'Alice' and 'Bob' and logging the return value
+console.log(sayHello('Alice'));
+console.log(sayHello('Bob'));
+
+// Function to calculate the sum of two numbers
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+
+console.log(addNumbers(5, 3)); 
+console.log(addNumbers(-2, 7)); 
+console.log(addNumbers(10, -4)); 
